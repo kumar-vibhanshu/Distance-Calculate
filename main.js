c28f6d614e0c9c7356eb380b893fab32cd1a38bd
Kumar vibhanshu <vibhanshumonty@gmail.com>
2020-12-13 13:42:21 +0530

    update main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ function calcRoute() {
                 ".<br/><strong>Email</strong>  " + document.getElementById('customer_email').value + 
                 ".<br/><strong>phone</strong>  " + document.getElementById('customer_phone').value + 
                 ".<br/><strong>Driving distance:</strong>  " + distance.text + 
-                ".<br/><strong>Base Cost:</strong>  " + basePrice + 
+                ".<br/><strong>Base Cost:</strong>  " + console.log(basePrice) + 
                 "</div>");
 
             directionsDisplay.setDirections(result);
