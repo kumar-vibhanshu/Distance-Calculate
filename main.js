4e64f7af26c24603e61f4f27b157320e551bb1d3
Kumar vibhanshu <vibhanshumonty@gmail.com>
2020-12-13 13:08:45 +0530

    update main.js

--- main.js
+++ main.js
@@ -45,11 +45,23 @@ function calcRoute() {
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
             
+
+            //Blue Collar City Base
             var basePrice;
             var city = document.getElementById('city').selectedIndex;
+            var service_level = document.getElementById('service_level').selectedIndex;
             if (city === 'Reno') {
-                basePrice = 275;
-                console.log(basePrice);
+                if(service_level === 'Same Day'){
+                    basePrice = 18;
+                    console.log(basePrice);
+                }else if(service_level === 'Rush'){
+                    basePrice = 25;
+                    console.log(basePrice);
+                }else if(service_level === 'Next Day'){
+                    basePrice = 15;
+                    console.log(basePrice);
+                }
+                
             } else if (city === 'Palm Springs') {
                 basePrice = 325;
                 console.log(basePrice);
